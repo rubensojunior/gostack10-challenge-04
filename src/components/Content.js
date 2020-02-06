c64b680e9ac90c70072c6bfc167f1b8086ffa4f6
rubensojunior <rubensojunior6@gmail.com>
2020-02-06 12:11:31 -0300

    adding components

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+import Feed from './Feed'
+
+class Content extends Component{
+  render(){
+    return(
+      <Feed />
+    )
+  }
+}
+
+export default Content
